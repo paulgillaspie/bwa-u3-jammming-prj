ec669f6032daeb121caa06a9bbe52623ed368049
Paul Gillaspie <paul.gillaspie@gmail.com>
2018-04-01 02:47:28 -0700

    Add files via upload

---
+++ src/Components/SearchResults/SearchResults.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './SearchResults.css';
+import TrackList from "../tracklist/TrackList";
+
+class SearchResults extends React.Component {
+  render() {
+    return (
+      <div className="SearchResults">
+          <h2>Results</h2>
+          <TrackList tracks={this.props.searchResults} onAdd={this.props.onAdd} shouldAdd={true}/>
+      </div>
+    );
+  }
+}
+
+export default SearchResults;
